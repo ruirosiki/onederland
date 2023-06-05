384ee14953224255d0a9eb2912073587f4afde9c
ruirosiki <mr.andrew.corbin@icloud.com>
2023-06-05 07:49:01 -0700

    //Fix variable names

--- main_app/models.py
+++ main_app/models.py
@@ -2,28 +2,27 @@ from django.db import models
 from django.urls import reverse
 from datetime import date
 
-MEALTYPES = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner'),
+MEAL_TYPES = (
+    ("B", "Breakfast"),
+    ("L", "Lunch"),
+    ("D", "Dinner"),
 )
+
 
 # Create your models here.
 class Meal(models.Model):
-    date = models.DateField('meal date')
-    mealtype = models.CharField(
-        max_length=1,
-        choices=MEALTYPES,
-        default=MEALTYPES[0][0]
+    date = models.DateField("meal date")
+    meal_type = models.CharField(
+        max_length=1, choices=MEAL_TYPES, default=MEAL_TYPES[0][0]
     )
-    
+
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'meal_id': self.id})
+        return reverse("detail", kwargs={"meal_id": self.id})
 
     #
     def __str__(self):
-        return f"{self.get_mealtype_display()} on {self.date}"
-    
+        return f"{self.get_meal_type_display()} on {self.date}"
+
     # filtered list with recent dates first
-    class Meta: 
-        ordering = ['-date']
+    class Meta:
+        ordering = ["-date"]
