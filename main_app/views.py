168cf957d6ae5b7bc5a90020f718d7e22a612989
ruirosiki <mr.andrew.corbin@icloud.com>
2023-06-06 06:31:23 -0700

    //Code API - Calorie Ninja

--- main_app/views.py
+++ main_app/views.py
@@ -1,63 +1,75 @@
 from django.shortcuts import render, redirect
 from .models import Meal
-from django.contrib.auth import login 
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+import requests
 
+api_url = "https://api.calorieninjas.com/v1/nutrition?query="
+query = "12oz new york strip"
+response = requests.get(
+    api_url + query, headers={"X-Api-Key": "WckbYIY9LQLe8m72V8rEYw==YCE4Hjwib4uDUhdI"}
+)
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
 
 
 # Create your views here.
 
+
 # home view/controller function
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 # about view/controller function
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 # meals view/controller function
 def meals_index(request):
     meals = Meal.objects.filter(user=request.user)
-    return render(request, 'meals/index.html', {
-        'meals': meals
-    })
+    return render(request, "meals/index.html", {"meals": meals})
+
 
 def meals_details(request, meal_id):
     meal = Meal.objects.get(id=meal_id)
-    return render(request, 'meals/detail.html', {
-        'meal':meal
-    })
+    return render(request, "meals/detail.html", {"meal": meal})
 
 
 class MealCreate(LoginRequiredMixin, CreateView):
     model = Meal
-    fields = ['date', 'meal_type']
+    fields = ["date", "meal_type"]
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
+
 class MealUpdate(LoginRequiredMixin, UpdateView):
     model = Meal
-    fields = ['date', 'meal_type']
+    fields = ["date", "meal_type"]
+
 
 class MealDelete(LoginRequiredMixin, DeleteView):
     model = Meal
-    success_url = '/meals/'
+    success_url = "/meals/"
 
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
+    error_message = ""
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            error_message = 'Invalid sign up - try again'
+            error_message = "Invalid sign up - try again"
     form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
-
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
